45ab41285c186b44d30bfb4c6f0d76269ebee3a4
eduardz1 <54178827+eduardz1@users.noreply.github.com>
2022-11-05 19:28:25 +0100

    pusho perché devo switcahre sulla WSL

---
+++ server/src/main/java/jmail/server/models/Email.java
@@ -0,0 +1,17 @@
+package jmail.server.models;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
+
+import lombok.NonNull;
+
+public record Email (
+    String subject, 
+    String body, 
+    @NonNull String sender, 
+    @NonNull List<String> recipients, 
+    @NonNull Date date
+    ) implements Serializable {
+
+}
