1dd000a8891f422c9b7c4fadef04bd4da9e1ea74
Eduard <occhipinti.eduard@icloud.com>
2022-11-05 19:56:59 +0100

    added library for models

--- lib/src/main/java/jmail/models/Email.java
+++ lib/src/main/java/jmail/models/Email.java
@@ -1,4 +1,4 @@
-package jmail.server.models;
+package jmail.models;
 
 import java.io.Serializable;
 import java.sql.Date;
---
+++ lib/src/main/java/jmail/models/User.java
@@ -0,0 +1,12 @@
+package jmail.models;
+
+import lombok.Data;
+import lombok.NonNull;
+
+@Data
+public class User {
+    @NonNull private String Email;
+    @NonNull private String Name;
+    @NonNull private String Surname;
+    private String Avatar;
+}
